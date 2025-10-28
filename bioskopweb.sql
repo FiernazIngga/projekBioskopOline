d560586331c17f57824253c20987b5af897c78fe
FiernazIngga <inggapratama86@gmail.com>
2025-10-28 07:41:45 +0700

    kjkj

--- bioskopweb.sql
+++ bioskopweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 27 Okt 2025 pada 10.01
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Oct 28, 2025 at 12:41 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,28 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `role`
+-- Table structure for table `admin_data`
+--
+
+CREATE TABLE `admin_data` (
+  `id_admin` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin_data`
+--
+
+INSERT INTO `admin_data` (`id_admin`, `username`, `password`) VALUES
+(1, 'upn', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
-  `id_role` int(11) NOT NULL,
+  `id_role` int NOT NULL,
   `id_user` varchar(255) NOT NULL,
-  `role_user` varchar(11) NOT NULL DEFAULT 'Free',
+  `role_user` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Free',
   `expired_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `role`
+-- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`id_role`, `id_user`, `role_user`, `expired_at`) VALUES
-(1, 'usr_68fa3463db0ee_20251023', 'Free', NULL),
+(1, 'usr_68fa3463db0ee_20251023', 'Premium', '2025-12-25 15:36:28'),
 (2, 'usr_68fa3637bc364_20251023', 'Free', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -55,90 +74,94 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `foto_profil` varchar(100) NOT NULL DEFAULT 'default.png'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `username`, `password`, `email`, `foto_profil`) VALUES
-('usr_68fa3463db0ee_20251023', 'Affanzaky', 'admin', '123', 'affan@gmail.com', 'usr_68fa3463db0ee_20251023_1761481711.jpg'),
+('usr_68fa3463db0ee_20251023', 'Prada Bimaya', 'admin', '123', 'prada@gmail.com', 'usr_68fa3463db0ee_20251023_1761228323.jpg'),
 ('usr_68fa3637bc364_20251023', 'Albertus Ali Edited', 'user', '123', 'albertus@gmail.com', 'default.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `video`
+-- Table structure for table `video`
 --
 
 CREATE TABLE `video` (
-  `id_video` int(11) NOT NULL,
+  `id` int NOT NULL,
   `judul` varchar(255) NOT NULL,
-  `nama_file` varchar(255) DEFAULT NULL,
-  `deskripsi` text DEFAULT NULL,
-  `tanggal_upload` datetime DEFAULT current_timestamp(),
-  `role` varchar(50) DEFAULT 'Free',
-  `jumlah_view` int(11) DEFAULT 0,
-  `jumlah_like` int(11) DEFAULT 0,
-  `rating` decimal(2,1) DEFAULT 0.0,
-  `id_user` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `sinopsis` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_video` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  `genre` varchar(100) DEFAULT NULL,
+  `durasi` varchar(20) DEFAULT NULL,
+  `tanggal_upload` datetime DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(50) DEFAULT 'basic',
+  `jumlah_view` int DEFAULT '0',
+  `jumlah_like` int DEFAULT '0',
+  `rating` int DEFAULT '0',
+  `trailer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `video`
+-- Dumping data for table `video`
 --
 
-INSERT INTO `video` (`id_video`, `judul`, `nama_file`, `deskripsi`, `tanggal_upload`, `role`, `jumlah_view`, `jumlah_like`, `rating`, `id_user`) VALUES
-(1, 'Cars 3', 'vid_68fe294af0245.mp4', 'njajal upload nagnggo carane AI', '2025-10-26 20:59:38', 'admin', 0, 0, 0.0, NULL);
+INSERT INTO `video` (`id`, `judul`, `sinopsis`, `file_video`, `thumbnail`, `genre`, `durasi`, `tanggal_upload`, `role`, `jumlah_view`, `jumlah_like`, `rating`, `trailer`) VALUES
+(24, 'affan', '123', '1761611475_bahan.mp4', '1761611475_for zoom.jpg', 'Action', '1 Jam 30 Menit', '2025-10-28 07:31:15', 'Premium', 0, 0, 0, '1761611475_bahan.mp4');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `role`
+-- Indexes for table `admin_data`
+--
+ALTER TABLE `admin_data`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id_role`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indeks untuk tabel `video`
+-- Indexes for table `video`
 --
 ALTER TABLE `video`
-  ADD PRIMARY KEY (`id_video`),
-  ADD KEY `fk_video_user` (`id_user`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `role`
+-- AUTO_INCREMENT for table `admin_data`
+--
+ALTER TABLE `admin_data`
+  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_role` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `video`
+-- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `video`
---
-ALTER TABLE `video`
-  ADD CONSTRAINT `fk_video_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
